b6f9b874e7ceccfe7f0c388c7de7bf82e12b2fab
Enrique <enrique.barra.arias@gmail.com>
2018-03-22 00:31:11 +0100

    step7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Panel } from 'react-bootstrap';
+
+const title = (
+  <h3>Turn</h3>
+);
 
 export default class Header extends React.Component {
     render() {
         return (
-          <header className="header">
-            {this.props.text}
-          </header>
+          <Panel className="turn" bsStyle="info">
+            <Panel.Heading>
+              <Panel.Title componentClass="h3">Turn</Panel.Title>
+            </Panel.Heading>
+            <Panel.Body>{this.props.text}</Panel.Body>
+          </Panel>
         );
     }
 
---
+++ src/components/Reset.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default class Reset extends React.Component {
+    constructor(props) {
+        super(props);
+        this.click = this.click.bind(this);
+    }
+
+    click() {
+        this.props.resetClick();
+    }
+
+    render() {
+        return(
+          <Button bsStyle="info" onClick={this.click}>Reset</Button>
+        );
+    }
+}
